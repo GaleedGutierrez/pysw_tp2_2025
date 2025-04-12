dbf61027d3a87eb22c2110d7b1a1bb5b6aadc58e
galeed gutierrez <galeedeveloper@gmail.com>
2025-04-11 23:42:29 -0300

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://galeedgutierrez.com',
+	base: 'pysw_tp2_2025',
 	experimental: {
 		svg: true,
 	},
